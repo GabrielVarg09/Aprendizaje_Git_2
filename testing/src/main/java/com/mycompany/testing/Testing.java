02796984d3ba775ea21c5d152d6289ffdb8ec138
GabrielVarg09 <gabvarg09@gmail.com>
2025-09-16 19:36:32 -0600

    base_de_mi_proyecto

---
+++ testing/src/main/java/com/mycompany/testing/Testing.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testing;
+
+/**
+ *
+ * @author XPC
+ */
+public class Testing {
+
+    public static void main(String[] args) {
+        int numero = 7;
+        
+        System.err.println("soy un numero" + " " +numero);
+       
+    }
+}
